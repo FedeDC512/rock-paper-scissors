7be93a786ecb6fa8ea09cbac00f88912d23af328
FedeDC512 <78120567+FedeDC512@users.noreply.github.com>
2024-02-15 00:57:01 +0100

    UI improved with HTML and CSS

--- myscripts.js
+++ myscripts.js
@@ -32,16 +32,29 @@
         } else return "This is a game of Rock Paper Scissors, you can't use \""+playerSelection+"\" here!"
     }
 
-    const buttons = document.querySelectorAll('button');
+    function rematchGame(){
+        gameState = true;
+        logsText.innerHTML = logsText.innerHTML + "<br>A new match has started!<br>";
+        mainText.innerHTML = "A new 5-round game of Rock Paper Scissors has started!<br>What is your choice?";
+        roundCountText.innerHTML = "The new match hasn't started yet."
+        playerScore = 0;
+        computerScore = 0;
+        roundCount = 0;
+    }
+
+    const buttons = document.querySelectorAll('.rps-button');
     const mainText = document.querySelector('#main-text');
     const logsText = document.querySelector('#logs');
     const roundCountText = document.querySelector('#round-count');
+    const rematch = document.querySelector('#rematch');
 
     let gameState = true;
     let roundCount = 0;
 
     let playerScore = 0;
     let computerScore = 0;
+
+    rematch.onclick = () => rematchGame();
     
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
@@ -49,22 +62,30 @@
                 roundCount++
                 if(roundCount < 5 ){
                     let finalDisplayMessage = playRound(button.id);
-                    logsText.textContent = logsText.textContent + " Round "+(roundCount) + ": " + finalDisplayMessage;
-                    mainText.textContent = finalDisplayMessage +" What is your choice for round " + (roundCount+1) + "?";
-                } else if(roundCount < 6 ){let finalDisplayMessage = playRound(button.id);
-                    logsText.textContent = logsText.textContent + " Round "+(roundCount) + ": " + finalDisplayMessage;
-                    mainText.textContent = finalDisplayMessage +" The match is over!";
-                    roundCountText.textContent = "The Player's score is "+playerScore+" and the Computer's score is "+computerScore;
-                    logsText.textContent = logsText.textContent + " Final Score: " +roundCountText.textContent + ".";
+                    logsText.innerHTML = logsText.innerHTML + "Round "+(roundCount) + ": " + finalDisplayMessage + "<br>";
+                    mainText.innerHTML = finalDisplayMessage +"<br>What is your choice for round " + (roundCount+1) + "?";
+                } else if(roundCount < 6 ){
+                    let finalDisplayMessage = playRound(button.id);
+                    logsText.innerHTML = logsText.innerHTML + "Round "+(roundCount) + ": " + finalDisplayMessage + "<br>";
+                    mainText.innerHTML = finalDisplayMessage +"<br>The match is over!";
                     gameState = false;
                 } else {
                     console.log("Something went wrong with roundCount: " + roundCount);
                 }
 
-                if((mainText.textContent).includes("you won")) playerScore++
-                else if ((mainText.textContent).includes("you lost")) computerScore++
+                if((mainText.innerHTML).includes("you won!")) playerScore++
+                else if ((mainText.innerHTML).includes("you lost!")) computerScore++
 
-            } else mainText.textContent = "No game is currently playing right now, do you want a rematch?"
+                if(roundCount == 5){
+                    if(playerScore < computerScore){
+                        roundCountText.innerHTML = "The Player's score is "+playerScore+" and the Computer's score is "+computerScore+". You lost the game!";
+                    } else if (playerScore > computerScore){
+                        roundCountText.innerHTML = "The Player's score is "+playerScore+" and the Computer's score is "+computerScore+". You won the game!";
+                    } else roundCountText.innerHTML = "The Player's score is "+playerScore+" and the Computer's score is "+computerScore+". It's a tie!";
+                    logsText.innerHTML = logsText.innerHTML + "Final Score: " +roundCountText.innerHTML + "<br>";
+                } else roundCountText.innerHTML = "The Player's score is "+playerScore+" and the Computer's score is "+computerScore+".";
+
+            } else mainText.innerHTML = "No game is currently playing right now.<br>Do you want to start a rematch?"
             });
         });
 
